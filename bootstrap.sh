5dd6ec49e823abf6be5680ae5bcf30cd1cf0dda9
tom <ster.bzh@gmail.com>
2016-04-06 23:46:59 +0200

    Delete bootstrap.sh

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-# TODO Copy files into ~/
-
-# Tmux plugin manager
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux source-file ~/.tmux.conf
